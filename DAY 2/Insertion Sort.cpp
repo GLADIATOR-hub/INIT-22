b9226af84b564701f5b9262cbaab19e557670533
Ayush489 <86658135+Ayush489@users.noreply.github.com>
2021-06-29 20:22:12 +0530

    Update Insertion Sort.cpp

--- DAY 2/Insertion Sort.cpp
+++ DAY 2/Insertion Sort.cpp
@@ -1,3 +1,5 @@
+// INSERTION SORT
+
 #include <iostream>
 using namespace std;
 
@@ -8,12 +10,12 @@ void printArray(int array[], int size) {
   cout << endl;
 }
 
-void insertionSort(int array[], int size) {
+void insertionSort(int array[], int size) { //SORTING FUNCTION
   for (int step = 1; step < size; step++) {
-    int key = array[step];
+    int key = array[step];              // KEY ELEMENT
     int j = step - 1;
 
-    while (key < array[j] && j >= 0) {
+    while (key < array[j] && j >= 0) { //COMPARISON WITH KEY ELEMENT
       array[j + 1] = array[j];
       --j;
     }
@@ -33,9 +35,9 @@ int main()
       cin>>arr[i];
    }
    cout<<endl;
-    insertionSort(arr, n); 
+    insertionSort(arr, n); //CALLING INSERTION SORT FUNCTION
     cout<<"Sorted array: \n"; 
-    printArray(arr, n); 
+    printArray(arr, n); // PRINTING SORTED ARRAY
     delete[] arr;
     return 0; 
 }
